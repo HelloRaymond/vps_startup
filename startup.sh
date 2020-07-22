ec6aeba3f7b69da8b00a58bf4dfe53f5ddf6df18
raymond <xurui1995@hotmail.com>
2020-07-22 13:52:07 +0800

    删除未经测试的代码

--- startup.sh
+++ startup.sh
@@ -32,7 +32,7 @@ checkUser()
 {
     if [ $UID -ne 0 ]; then
         printColor red '此脚本需要以管理员权限运行' >> /dev/stderr
-        printColor yellow "例如：\"sudo $0\""
+        printColor yellow "例如：\"$0\""
         exit 1
     fi
 }
@@ -97,15 +97,9 @@ printWelcome()
 #确认开始部署
 confirmOperation()
 {
-    if [ $(uname -r | awk -F "." '{print $1}') -eq 3 ]; then
-        printColor yellow "即将安装锐速、Docker-CE，
-        配置ssh端口8022，密钥登录、禁止密码登录，
-        并配置防火墙、更新系统"
-    else
-        printColor yellow "即将安装Docker-CE、开启bbr，
-        配置ssh端口8022，密钥登录、禁止密码登录，
-        并配置防火墙、更新系统"
-    fi
+    printColor yellow "即将开启bbr加速、安装Docker-CE，
+    配置ssh端口8022，密钥登录、禁止密码登录，
+    并配置防火墙、更新系统"
     read -p "是否继续？(y or n)" confirm
     if [ $confirm != 'y' ]; then
         printColor red '终止安装！' >> /dev/stderr
@@ -119,12 +113,12 @@ updateSystem()
     if [ $PackageManager = 'apt' ]; then
         apt-get update -y
         apt-get upgrade -y
-        apt-get install -y wget curl net-tools tcl tk expect
+        apt-get install -y wget curl net-tools tcl tk expect sudo ufw
     fi
     if [ $PackageManager = 'yum' ]; then
         yum update -y
         yum upgrade -y
-        yum install -y wget curl net-tools.x86_64 tcl tclx tcl-devel expect
+        yum install -y wget curl net-tools.x86_64 tcl tclx tcl-devel expect sudo
     fi
 }
 
@@ -141,11 +135,11 @@ installDockerCE()
             printColor yellow 'apt安装docker'
             apt-get install -y apt-transport-https ca-certificates software-properties-common
             if [ $1 = 'aliyun' ]; then
-                curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
-                sudo add-apt-repository -y "deb [arch=$(dpkg --print-architecture)] https://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+                curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/$Distro/gpg | apt-key add -
+                add-apt-repository -y "deb [arch=$(dpkg --print-architecture)] https://mirrors.aliyun.com/docker-ce/linux/$Distro $(lsb_release -cs) stable"
             else
-                curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-                sudo add-apt-repository -y "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+                curl -fsSL https://download.docker.com/linux/$Distro/gpg | apt-key add -
+                add-apt-repository -y "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/$Distro $(lsb_release -cs) stable"
             fi
             apt-get update -y
             apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
@@ -170,12 +164,6 @@ installDockerCE()
     fi
 }
 
-#部署Docker容器
-deployContainers()
-{
-    bash ./docker_deploy.sh
-}
-
 #配置SSH
 configSSH()
 {
@@ -192,8 +180,8 @@ configSSH()
     echo 'Subsystem       sftp    /usr/lib/openssh/sftp-server' >> /etc/ssh/sshd_config
     echo 'AuthorizedKeysFile      .ssh/authorized_keys' >> /etc/ssh/sshd_config
 
-    if [ ! -f "~/.ssh" ]; then
-        mkdir ~/.ssh
+    if [ ! -d "~/.ssh" ]; then
+        mkdir -p ~/.ssh
     fi
     if [ ! -f "./authorized_keys.pub" ]; then
         ssh-keygen -t rsa -N '' -f '~/.ssh/authorized_keys'
@@ -201,6 +189,7 @@ configSSH()
     else
         cp -rf ./authorized_keys.pub ~/.ssh/authorized_keys
         sed 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+        printColor green "已部署公钥文件authorized_keys.pub\n若要禁止密码登录，请执行 sed 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config 命令"
     fi
     printColor green 'SSH端口已更改为8022'
     systemctl restart sshd
@@ -209,7 +198,7 @@ configSSH()
 #配置防火墙
 configFirewalls()
 {
-    if [ $Distro = 'Ubuntu' ]; then
+    if [ $PackageManager = 'apt' ]; then
         ufw status
         if [ $? -ne 0 ]; then
             apt-get install ufw
@@ -218,19 +207,17 @@ configFirewalls()
         ufw disable
         echo y | ufw reset
         ufw default deny incoming
-        # for port in 8022/tcp 80/tcp 443/tcp 54269/tcp 54268/udp 54267/tcp 7788/tcp 7789/tcp 7789/udp;
         for port in 8022/tcp;
         do ufw allow $port; done
         ufw reload
 		echo y | ufw enable
     fi
-    if [ $Distro = 'CentOS' ]; then
+    if [ $PackageManager = 'yum' ]; then
         firewall-cmd --state
         if [ $? -ne 0 ]; then
             yum install firewalld -y
         fi
         printColor yellow '正在配置firewall...'
-        # for port in 8022/tcp 80/tcp 443/tcp 54269/tcp 54268/udp 54267/tcp 7788/tcp 7789/tcp 7789/udp;
         for port in 8022/tcp;
         do echo y | firewall-cmd --zone=public --add-port $port --permanent; done
         firewall-cmd --reload
@@ -241,37 +228,14 @@ configFirewalls()
 #配置网络加速
 configNetworkSpeeder()
 {
-    version=`uname -r|awk -F"." '{print $1}'`
-    if [ $version -eq 3 ]; then
-        service serverSpeeder status
-        if [ $? -eq 1 ]; then
-            printColor green '已经安装锐速' >> /dev/stderr
-        fi
-        printColor yellow "正在安装锐速..."
-        CHECKSYSTEM=https://raw.githubusercontent.com/91yun/serverspeeder/test/serverspeederbin.txt
-        wget $CHECKSYSTEM --no-check-certificate -O serverspeederbin.txt || { echo "Error downloading file, please try again later.";exit 1;}
-        #判断是否有完全匹配的内核
-        grep -q "$Distro/[^/]*/$Kernel_Ver/$OS_Bit" serverspeederbin.txt
-        if [ $? -eq 0 ]; then
-            #如果完全匹配，则取的内核版本
-            wget -N --no-check-certificate https://github.com/91yun/serverspeeder/raw/master/serverspeeder.sh && bash serverspeeder.sh
-            /etc/init.d/serverSpeeder start
-        else
-            printColor red '内核版本不符，正在降级内核版本...' >> /dev/stderr
-            rpm -ivh http://soft.91yun.pw/ISO/Linux/CentOS/kernel/kernel-3.10.0-229.1.2.el7.x86_64.rpm --force
-            printColor red '请重新启动后再次运行此脚本'
-            destroySelfFlag=0;
-        fi
-    elif [ $version -eq 4 ]; then
-        bbr_status=`sysctl -a|grep net.ipv4.tcp_congestion_control|awk '{print $3}'`
-        if [ bbr_status = 'bbr' ]; then
-            printColor green "已开启bbr"
-        else
-            printColor yellow "正在开启bbr加速..."
-            sysctl -w "net.core.default_qdisc=fq"
-            sysctl -w "net.ipv4.tcp_congestion_control=bbr"
-            sysctl -p
-        fi
+    bbr_status=`sysctl -a|grep net.ipv4.tcp_congestion_control|awk '{print $3}'`
+    if [ bbr_status = 'bbr' ]; then
+        printColor green "已开启bbr"
+    else
+        printColor yellow "正在开启bbr加速..."
+        sysctl -w "net.core.default_qdisc=fq"
+        sysctl -w "net.ipv4.tcp_congestion_control=bbr"
+        sysctl -p
     fi
 }
 
@@ -292,7 +256,6 @@ main()
     confirmOperation
     updateSystem
     installDockerCE
-    # deployContainers
     configSSH
     configFirewalls
     configNetworkSpeeder
@@ -301,6 +264,6 @@ main()
 }
 
 main
-# destroySelf
-# reboot
+destroySelf
+reboot
 
